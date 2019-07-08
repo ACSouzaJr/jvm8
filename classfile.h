4dcad751c00e065e8b60d521f8c068d57899e921
ACSouzaJr <junioracs2000@hotmail.com>
2019-07-08 20:16:02 -0300

    correcao

--- classfile.h
+++ classfile.h
@@ -39,3 +39,4 @@ u4 ClassLoader(char *class_name);
 void execute_gvm();
 method_info* find_method(ClassFile *cf, char* method);
 u2 find_class(char* class_name);
+field_info* find_field(ClassFile *cf, char *field_name, char* field_desc);
