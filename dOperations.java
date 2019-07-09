5b74cdf6d5c3b2f11ed32acdb34d4b4f608e23c4
nicknish09 <nnmarques97@gmail.com>
2019-07-09 11:25:06 -0300

    doperations com prints

--- dOperations.java
+++ dOperations.java
@@ -2,11 +2,16 @@ class dOperations {
   public static double main(String[] s) {
     double a = 5.2, b = 4.3, c, d = 0.0, e = 1.0;
     c = a - b;
+    System.out.println(c);
     c = a * b;
+    System.out.println(c);
     c = a / b;
+    System.out.println(c);
     c = a % b;
+    System.out.println(c);
     if(a > b){
       c = -a;
+      System.out.println(c);
     }
     c = a + b;
     System.out.println(c);
